71a69b938ced2c1843d0be2193431cc4dcbcae9c
gushchinvadim <gushchinvadim@gmail.com>
2025-01-12 17:36:03 +0300

    third commit

--- dict.py
+++ dict.py
@@ -1,10 +1,27 @@
 import requests
-import json
-import pprint
+# import json
+# import pprint
+#
+# url = 'https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/all.json'
+# response = requests.get(url)
+# heroes = response.json()
+#
+# with open('superherofile.json', "w") as f:
+#     json.dump(heroes, f)
+#
+#     dct = {'a': 1, 'b': 2}
+#     lst1 = ['c', 'd']
+#     lst2 = [3, 4]
+#     for i in range(0, 2):
+#
+#         key = lst1[i]
+#         value = lst2[i]
+#         dct[key] = value
+#     print(dct)
 
-url = 'https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/all.json'
-response = requests.get(url)
-heroes = response.json()
+# url = "https://httpbin.org/get"
+# resp = requests.get(url)
+# print(resp.json())
 
-with open('superherofile.json', "w") as f:
-    json.dump(heroes, f)
+r = requests.get("https://netology.ru/")
+print(r)
--- hollidays.py
+++ hollidays.py
@@ -1,29 +1,64 @@
 import requests
 import time
 
+sity_list = ['Leeds', 'London', 'Liverpool', 'Manchester', 'Oxford', 'Edinburgh', 'Norwich', 'York']
 
-def find_uk_city(coordinates: list) -> str:
-    API = '65ae97b123696043220487avude3e26'
-    URL = 'https://geocode.maps.co/reverse?'
+# _coordinates = [
+#     ('55.7514952', '37.618153095505875'),
+#     ('52.3727598', '4.8936041'),
+#     ('53.4071991', '-2.99168')
+# ]
+# for coord in _coordinates:
+#     lat, lon = coord[0], coord[1]
+# url = "https://geocode.maps.co/reverse"
+# api_key = '67839b9207d1c680106760ajuccbbe3'
+# param = {
+#     'lat': lat,
+#     'lon': lon,
+#     'key': api_key
+# }
+# response = requests.get(url=url, params=param).json()
+# city = response['address']['city']
+# if city in sity_list:
+#     print(city)
+# else:
+#     time.sleep(10)
+#          continue
 
-    uk_city = ['Leeds', 'London', 'Liverpool', 'Manchester', 'Oxford', 'Edinburgh', 'Norwich', 'York']
-
-    for lat, lon in coordinates:
-        response = requests.get(f'{URL}lat={lat}&lon={lon}&key={API}')
-        data = response.json()
-        city = data.get('address', {}).get('city')
-
-        if city in uk_city:
-            return city
-        else:
-            time.sleep(15)
-            continue
 
 
-if __name__ == '__main__':
-    _coordinates = [
-        ('55.7514952', '37.618153095505875'),
-        ('52.3727598', '4.8936041'),
-        ('53.4071991', '-2.99168')
-    ]
-    assert find_uk_city(_coordinates) == 'Liverpool'
+# def find_uk_city(coordinates:list) -> str:
+#     """Ваш код здесь"""
+#     ...
+#
+#
+# if __name__ == '__main__':
+#     _coordinates = [
+#         ('55.7514952', '37.618153095505875'),
+#         ('52.3727598', '4.8936041'),
+#         ('53.4071991', '-2.99168')
+#     ]
+#     assert find_uk_city(_coordinates) == 'Liverpool'
+sity_dict = {}
+api_key = '67839b9207d1c680106760ajuccbbe3'
+
+for adress in sity_list:
+    pass
+    url = 'https://geocode.maps.co/search'
+    param = {
+        'q': adress,
+        'key': api_key
+
+    }
+    response = requests.get(url=url, params=param).json()
+    coord_dict = response[0]
+    coord = coord_dict['lat'], coord_dict['lon']
+
+    print(adress, coord)
+
+
+# url = f'https://geocode.maps.co/reverse?lat={latitude}&lon={longitude}&api_key={api_key}'
+# longitude = -2.99168
+# url = f'https://geocode.maps.co/reverse?lat={latitude}&lon={longitude}&api_key={api_key}'
+# payload = {}
+# headers = {}
